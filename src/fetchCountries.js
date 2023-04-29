58d5c52bc2451d15b46a2655996c41c8a43bbc7f
NataliaBielik1 <116969531+NataliaBielik1@users.noreply.github.com>
2023-04-29 20:23:09 +0300

    Start 11

--- src/fetchCountries.js
+++
@@ -1,7 +0,0 @@
-export default async function fetchCountries(name) {
-  return await fetch(
-    "https://restcountries.com/v3.1/name/" +
-      name +
-      "?fields=name,capital,population,flags,languages"
-  );
-}
